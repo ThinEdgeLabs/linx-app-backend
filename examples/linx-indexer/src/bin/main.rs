5f6f52aa2494f7a675ab0150f9a97be00a256469
Radu Ciobanu <radu.ciobanu22@gmail.com>
2025-09-19 14:28:10 +0800

    Updated main

--- examples/linx-indexer/src/bin/main.rs
+++ examples/linx-indexer/src/bin/main.rs
@@ -1,21 +1,26 @@
 use std::collections::HashMap;
 
 use linx_indexer::{
-    processors::contract_call_processor, processors::dex_processor, processors::transfer_processor,
-    routers::AccountTransactionApiModule,
+    processors::{contract_call_processor, dex_processor, lending_processor, transfer_processor},
+    routers::{AccountTransactionsRouter, LendingRouter},
 };
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     dotenvy::dotenv().ok();
 
+    // Adding processor factories
     let mut processor_factories = HashMap::new();
     processor_factories.insert("transfers".to_string(), transfer_processor::processor_factory());
     processor_factories
         .insert("contract_calls".to_string(), contract_call_processor::processor_factory());
     processor_factories.insert("dex".to_string(), dex_processor::processor_factory());
-    let router = Some(AccountTransactionApiModule::register());
-    bento_cli::run_command(processor_factories, router, true).await?;
+    processor_factories.insert("lending".to_string(), lending_processor::processor_factory());
+
+    // Adding routes
+    let router = AccountTransactionsRouter::register().merge(LendingRouter::register());
+
+    bento_cli::run_command(processor_factories, Some(router), true).await?;
 
     Ok(())
 }
