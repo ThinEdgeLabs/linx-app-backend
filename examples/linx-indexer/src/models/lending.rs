7b76f2e0538c52a134701b34b6d2dba9a73b6239
Radu Ciobanu <radu.ciobanu22@gmail.com>
2025-09-20 23:19:06 +0800

    LendingEvent model

--- examples/linx-indexer/src/models/lending.rs
+++ examples/linx-indexer/src/models/lending.rs
@@ -5,6 +5,7 @@ use chrono::NaiveDateTime;
 
 use diesel::prelude::{AsChangeset, Insertable, Queryable};
 use serde::Serialize;
+use serde_json::Value;
 use utoipa::ToSchema;
 
 #[derive(Queryable, Insertable, Debug, Clone, Serialize, AsChangeset, ToSchema)]
@@ -12,7 +13,6 @@ use utoipa::ToSchema;
 #[diesel(check_for_backend(diesel::pg::Pg))]
 pub struct Market {
     pub id: String,
-    #[diesel(column_name = marketcontractid)]
     pub market_contract_id: String,
     pub collateral_token: String,
     pub loan_token: String,
@@ -22,4 +22,42 @@ pub struct Market {
     pub ltv: BigDecimal,
     #[schema(value_type = String)]
     pub created_at: NaiveDateTime,
+}
+
+#[derive(Queryable, Insertable, Debug, Clone, Serialize, AsChangeset, ToSchema)]
+#[diesel(table_name = schema::lending_events)]
+#[diesel(check_for_backend(diesel::pg::Pg))]
+pub struct LendingEvent {
+    pub id: i64,
+    pub market_id: String,
+    pub event_type: String,
+    pub token_id: String,
+    pub on_behalf: String,
+    #[schema(value_type = String)]
+    pub amount: BigDecimal,
+    pub transaction_id: String,
+    pub event_index: i32,
+    #[schema(value_type = String)]
+    pub block_time: NaiveDateTime,
+    #[schema(value_type = String)]
+    pub created_at: NaiveDateTime,
+    #[serde(skip)]
+    #[schema(ignore)]
+    pub fields: Value,
+}
+
+#[derive(Insertable, Debug, Clone)]
+#[diesel(table_name = schema::lending_events)]
+#[diesel(check_for_backend(diesel::pg::Pg))]
+pub struct NewLendingEvent {
+    pub market_id: String,
+    pub event_type: String,
+    pub token_id: String,
+    pub on_behalf: String,
+    pub amount: BigDecimal,
+    pub transaction_id: String,
+    pub event_index: i32,
+    pub block_time: NaiveDateTime,
+    pub created_at: NaiveDateTime,
+    pub fields: Value,
 }
