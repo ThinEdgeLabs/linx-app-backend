055729bdf6e58679b89e775efc29316fa30ea1d5
Radu Ciobanu <radu.ciobanu22@gmail.com>
2025-11-12 21:07:28 +0800

    Points endpoints

--- examples/linx-indexer/src/bin/main.rs
+++ examples/linx-indexer/src/bin/main.rs
@@ -2,7 +2,7 @@ use std::collections::HashMap;
 
 use linx_indexer::{
     processors::{contract_call_processor, dex_processor, lending_processor, transfer_processor},
-    routers::{AccountTransactionsRouter, LendingRouter},
+    routers::{AccountTransactionsRouter, LendingRouter, PointsRouter},
 };
 
 #[tokio::main]
@@ -18,7 +18,9 @@ async fn main() -> anyhow::Result<()> {
     processor_factories.insert("lending".to_string(), lending_processor::processor_factory());
 
     // Adding routes
-    let router = AccountTransactionsRouter::register().merge(LendingRouter::register());
+    let router = AccountTransactionsRouter::register()
+        .merge(LendingRouter::register())
+        .merge(PointsRouter::register());
 
     bento_cli::run_command(processor_factories, Some(router), true).await?;
 
