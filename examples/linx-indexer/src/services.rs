37daa801d70bb9979f3afe58da9941151bf7e7e8
Radu Ciobanu <radu.ciobanu22@gmail.com>
2025-12-23 23:31:01 +0800

    Updated services.rs

--- examples/linx-indexer/src/services.rs
+++ examples/linx-indexer/src/services.rs
@@ -1,11 +1,11 @@
 pub mod deposit_snapshot_service;
 pub mod gap_detection_service;
-// pub mod market_state_snapshot_service;
+pub mod market_state_snapshot_service;
 pub mod points_calculator_service;
 
 pub use deposit_snapshot_service::*;
 pub use gap_detection_service::*;
-// pub use market_state_snapshot_service::*;
+pub use market_state_snapshot_service::*;
 pub use points_calculator_service::*;
 
 pub mod price {
