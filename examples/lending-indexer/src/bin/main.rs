662c0c739882245e4b74666771a071302232430f
Radu Ciobanu <radu.ciobanu22@gmail.com>
2026-01-11 23:47:37 +0800

    Removed unused code

--- examples/lending-indexer/src/bin/main.rs
+++
@@ -1,8 +0,0 @@
-use std::collections::HashMap;
-#[tokio::main]
-async fn main() -> anyhow::Result<()> {
-    let mut processor_factories = HashMap::new();
-    processor_factories.insert("lending".to_string(), lending_example::processor_factory());
-    bento_cli::run_command(processor_factories, None, true).await?;
-    Ok(())
-}
